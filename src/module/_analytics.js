738f18fe4cc04e9b82fd3d88e2c146c236d591a5
bhsd <2545473905@qq.com>
2022-03-13 22:47:35 -0700

    test: stop collecting user information

--- src/module/_analytics.js
+++ src/module/_analytics.js
@@ -1,32 +1,29 @@
-const { analyticsApi } = require('./api')
-const { preference } = require('./preference')
+// const { analyticsApi } = require('./api')
+// const { preference } = require('./preference')
 const { config } = require('./util')
-const version = require('./version')
+// const version = require('./version')
 
 /**
  * @module _analytics 提交统计信息模块
  * @param {string} featID 模块ID，例如 quick_edit
  */
-function _analytics(featID) {
-  if (preference.get('doNotCollectMyInfo') === true) {
-    // console.info('[InPageEdit] 我们已不再收集您使用插件的信息。');
-    // return;
-  }
-  const submitData = {
-    siteUrl: getSiteID(),
-    siteName: config.wgSiteName,
-    userName: config.wgUserName,
-    featureID: featID,
-    ipeVersion: version,
-  }
-  $.ajax({
-    url: `${analyticsApi}/submit`,
-    data: submitData,
-    type: 'post',
-    dataType: 'json',
-  }).done(function (data) {
-    console.log('[InPageEdit] Analytics response', data)
-  })
+function _analytics(/* featID */) {
+  return
+  // const submitData = {
+  //   siteUrl: getSiteID(),
+  //   siteName: config.wgSiteName,
+  //   userName: config.wgUserName,
+  //   featureID: featID,
+  //   ipeVersion: version,
+  // }
+  // $.ajax({
+  //   url: `${analyticsApi}/submit`,
+  //   data: submitData,
+  //   type: 'post',
+  //   dataType: 'json',
+  // }).done(function (data) {
+  //   console.log('[InPageEdit] Analytics response', data)
+  // })
 }
 
 function getSiteID() {
