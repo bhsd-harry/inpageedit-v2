73f7df690db8c5c80f0d9effb5dd1ea75474b3a3
bhsd <2545473905@qq.com>
2022-03-15 00:33:05 -0700

    style: versionInfo.js

--- src/module/versionInfo.js
+++ src/module/versionInfo.js
@@ -7,40 +7,43 @@ const { updatelogsUrl, githubLink, aboutUrl } = require('./api')
  * @module versionInfo 版本信息模块
  * @description Show Update Logs Modal box
  */
-var versionInfo = function () {
+const versionInfo = function () {
   // 显示模态框
   ssi_modal.show({
     className: 'in-page-edit update-logs-modal',
-    title:
-      _msg('updatelog-title') +
-      ' - <span id="yourVersion">' +
-      version +
-      '</span>',
+    title: `${_msg(
+      'updatelog-title'
+    )} - <span id="yourVersion">${version}</span>`,
     content: $('<section>').append(
       $('<iframe>', {
-        style: 'margin: 0;padding: 0;width: 100%;height: 80vh;border: 0;',
         src: updatelogsUrl,
+      }).css({
+        margin: 0,
+        padding: 0,
+        width: '100%',
+        height: '80vh',
+        border: 0,
       })
     ),
     buttons: [
       {
         label: 'GitHub',
         className: 'btn btn-secondary',
-        method: function () {
+        method() {
           window.open(githubLink)
         },
       },
       {
         label: _msg('updatelog-about'),
         className: 'btn btn-secondary',
-        method: function () {
+        method() {
           window.open(aboutUrl)
         },
       },
       {
         label: _msg('close'),
         className: 'btn btn-primary',
-        method: function (a, modal) {
+        method(a, modal) {
           modal.close()
         },
       },
