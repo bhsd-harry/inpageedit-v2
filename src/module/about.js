923c32280a93f7fac87999b87789804f00d213c8
bhsd <2545473905@qq.com>
2022-03-15 01:18:23 -0700

    style: about.js

--- src/module/about.js
+++ src/module/about.js
@@ -6,14 +6,19 @@ const { aboutUrl } = require('./api')
  * @module about 关于插件模块
  * @description Show "What is" modal box of IPE2
  */
-var about = function () {
+const about = function () {
   ssi_modal.show({
     title: _msg('preference-about-label'),
     className: 'in-page-edit in-page-edit-about',
     content: $('<section>').append(
       $('<iframe>', {
-        style: 'margin: 0;padding: 0;width: 100%;height: 80vh;border: 0;',
         src: aboutUrl,
+      }).css({
+        margin: 0,
+        padding: 0,
+        width: '100%',
+        height: '80vh',
+        border: 0,
       })
     ),
   })
